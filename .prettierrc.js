d74f7c314a383a8d9a7a143777c5efef3cf2cc8e
Alieldin Ayman <alieldinayman@gmail.com>
2022-04-24 15:57:22 +0200

    Use single quotes and remove unnecessary ones from Prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    "semi": true,
-    "tabWidth": 4,
-    "printWidth": 120,
-    "singleQuote": true,
-    "trailingComma": "es5",
+    semi: true,
+    tabWidth: 4,
+    printWidth: 120,
+    singleQuote: true,
+    trailingComma: 'es5',
 
     // Vue-specific Rules
-    "vueIndentScriptAndStyle": true
+    vueIndentScriptAndStyle: true
 };
